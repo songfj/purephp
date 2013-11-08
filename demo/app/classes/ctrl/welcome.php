711e0bb1640ecf0d072271c19fe05ab41a18ba63
Javier Aguilar <mjaalnir@gmail.com>
2013-11-08 17:47:35 +0100

    welcome controller

--- demo/app/classes/ctrl/welcome.php
+++ demo/app/classes/ctrl/welcome.php
@@ -1,6 +1,6 @@
 <?php
 
-class ctrl_main {
+class ctrl_welcome {
 
     /**
      * 
--- demo/app/config.php
+++ demo/app/config.php
@@ -8,8 +8,8 @@ $config = array(
 
 // URL to callback mapping (in sequencial match order)
 // All the path expressions are based in expressjs ones
-pure::get('/', 'ctrl_main::index');
-pure::get('*', 'ctrl_main::handle');
+pure::get('/', 'ctrl_welcome::index');
+pure::get('*', 'ctrl_welcome::handle');
 
 // ALWAYS return an array
 return $config;
